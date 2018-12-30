25f4c19bdafe28f2fa237a5ac58c13947c201f73
zmichaelf <zmichaelf@hotmail.com>
2018-12-30 17:16:19 -0500

    my first method doesn't return anything

--- methods_prac.rb
+++ methods_prac.rb
@@ -1,5 +1,8 @@
 def my_first_method
     return 1 + 1 # code to be executed will sum 1+1
+    
 end
 
 my_first_method
+
+# why isn't Ruby running this program for me?  Is it bc we didn't tell it to print (puts) anything?
