ebfcfade4e63c6febd18ea35c307a8142160dbb3
zmichaelf <zmichaelf@hotmail.com>
2018-12-30 20:57:56 -0500

    added source for how I found how to do this, on line #9

--- exer3.rb
+++ exer3.rb
@@ -3,8 +3,10 @@ return num % 2 == 0
 
 end
 
-p is_even?(66)
+p is_even?(67)
 
-# Googling to see what I could find on even/odd:
+# Googling to see what I could find on even/odd: source: 
+# https://stackoverflow.com/questions/3517238/what-does-the-operator-do-in-ruby-in-n-2
+
 # % is the modulo operator. The result of counter % 2 is the remainder of counter / 2.
 # n % 2 is often a good way of determining if a number n is even or odd. If n % 2 == 0, the number is even (because no remainder means that the number is evenly divisible by 2); if n % 2 == 1, the number is odd.
