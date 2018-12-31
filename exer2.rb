856f231e786df15ce5e85d02e4e1257a07b52b81
zmichaelf <zmichaelf@hotmail.com>
2018-12-30 20:43:06 -0500

    couldn't complete exer3; can't find how to test for even/odd

---
+++ exer2.rb
@@ -0,0 +1,6 @@
+
+def negative?(num)
+    return num < 0
+end
+
+p negative?(-1)
---
+++ exer3.rb
@@ -0,0 +1,8 @@
+def is_even?(num)
+return num.to_i % odd?
+
+end
+
+p is_even?(2)
+
+# don't understand how to check for even/odd; found others asking, but no answers via Googling
