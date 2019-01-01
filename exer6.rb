afe2c4a700f6b0f50f16c7040b692798ccbd5544
zmichaelf <zmichaelf@hotmail.com>
2019-01-01 04:29:01 -0500

    Successfully completed exer6

---
+++ exer6.rb
@@ -0,0 +1,9 @@
+def FtoC(user_input)
+    
+    puts "#{user_input} degrees F is #{((user_input - 32.0) * (0.5556)).round} degrees C"
+
+end
+
+puts "Enter Temperature in Farenheit and I'll convert it to Celsius"
+user_input = gets.chomp.to_i
+FtoC(user_input)
